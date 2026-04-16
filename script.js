f3fd21501b25baaace2b704f941b1ab31be56329
Paco <francisco.mielgo@gmail.com>
2026-04-16 17:36:13 +0200

    Reconstrucción fase 3 completa

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+ // Espera a que cargue la página
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Marcar en el menú la página actual
+    const links = document.querySelectorAll("nav a");
+    const currentPage = window.location.pathname.split("/").pop();
+
+    links.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.style.color = "#999";
+        }
+    });
+
+});
