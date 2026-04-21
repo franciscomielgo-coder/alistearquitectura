4f93bfc3fe6652683984cc6539bc4290da7956be
Paco <francisco.mielgo@gmail.com>
2026-04-21 22:10:24 +0200

    incluye movil

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
- // Espera a que cargue la página
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Marcar en el menú la página actual
+    // Marcar página activa
     const links = document.querySelectorAll("nav a");
-    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+    const currentPage = window.location.href.split("/").pop();
 
     links.forEach(link => {
         if (link.getAttribute("href") === currentPage) {
-                link.classList.add("active");
+            link.classList.add("active");
         }
     });
+
+    // MENÚ HAMBURGUESA
+    const toggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    const overlay = document.querySelector('.menu-overlay');
+
+    if (toggle && nav && overlay) {
+
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('active');
+            toggle.classList.toggle('active');
+            overlay.classList.toggle('active');
+        });
+
+        overlay.addEventListener('click', () => {
+            nav.classList.remove('active');
+            toggle.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+
+        document.querySelectorAll('nav a').forEach(link => {
+            link.addEventListener('click', () => {
+                overlay.classList.remove('active');
+
+                setTimeout(() => {
+                    nav.classList.remove('active');
+                    toggle.classList.remove('active');
+                }, 50);
+            });
+        });
+    }
 
 });
