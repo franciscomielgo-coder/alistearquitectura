17b412ac890703ad0d9652ddef36469187320943
Paco <francisco.mielgo@gmail.com>
2026-04-22 17:59:42 +0200

    Segundas mejoras UX

--- script.js
+++ script.js
@@ -42,3 +42,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+// ANIMACIONES AL SCROLL
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+reveals.forEach(el => observer.observe(el));
